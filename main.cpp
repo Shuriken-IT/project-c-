d4ebfe8b29ade19e4f23477920f38ac8c7676f92
Hoàng Văn Mạnh <66491222+Shuriken-IT@users.noreply.github.com>
2020-06-11 05:48:04 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cstdint>
+#include <math.h>
+using namespace std;
+int main()
+{
+float x,y,x1;
+    cin>>x;
+    do{
+        x1=x;
+        y =(1/3)*(1/pow(pow((x+1),2),1/3));
+        x=y;
+        cout<<x;
+            }while((x-x1)>0.03);
+            cout<<x;
+}
